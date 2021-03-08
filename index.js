cc1d7aaeeec09adf0e356a5b970b389af1e52dc7
meg768 <meg768@users.noreply.github.com>
2021-03-08 11:36:23 +0100

    -

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-var Events = require("events");
 
-class MqttDispatch extends Events {
+var MQTT = require('mqtt');
 
-	constructor(mqtt) {
+function connect(host, options) {
 
-		super();
+	var mqtt = MQTT.connect(host, options);
+	var callbacks = {};
 
-		this.topics = [];
+	function isMatch(A, B) {
+		var args = {};
 
-		function isMatch(A, B) {
-			var args = {};
+		var A = A.split('/');
+		var B = B.split('/');
 
-			var A = A.split('/');
-			var B = B.split('/');
-		
-			if (A.length != B.length)
-				return null;
-		
-			for (let i = 0; i < A.length; i++) {
-				if (A[i] != B[i]) {
-					let match = B[i].match(/^:([a-zA-Z0-9]+)$/);
-		
-					if (!match)
-						return null;
-		
-					args[match[1]] = A[i];
-				}
+		if (A.length != B.length)
+			return null;
+
+		for (let i = 0; i < A.length; i++) {
+			if (A[i] != B[i]) {
+				let match = B[i].match(/^:([a-zA-Z0-9]+)$/);
+
+				if (!match)
+					return null;
+
+				args[match[1]] = A[i];
 			}
-
-			return args;
 		}
 
-		mqtt.on('message', (topic, message) => {
+		return args;
+	}
+
+	mqtt.on('message', (topic, message) => {
 	
-			message = message.toString();
+		message = message.toString();
 
-			this.topics.forEach((item) => {
-				let match = isMatch(topic, item);
+		for (const [item, fn] of Object.entries(callbacks)) {
+			let match = isMatch(topic, item);
 
-				if (match) {
-					this.emit(item, message, match);
-				}
-			});
-		});		
+			if (match) {
+				fn(message, match);
+			}
+		  
+		}
+	});
+
+	mqtt.dispatch = function(topic, fn) {
+		callbacks[topic] = fn;
 	}
 
-	on(topic, fn) {
-		this.topics.push(topic);
-		super.on(topic, fn);
-	}
+	return mqtt;
 }
 
-module.exports = MqttDispatch;
+module.exports.connect = connect
+module.exports.MqttClient = MQTT.MqttClient
+module.exports.Client = MQTT.MqttClient
+module.exports.Store = MQTT.Store
+
